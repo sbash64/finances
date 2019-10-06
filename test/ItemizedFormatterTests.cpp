f5a9a6a84203a1cea506b81bd0976a2a4049a4dc
Seth Bashford <sbash64@gmail.com>
2019-10-06 17:22:36 -0500

    add test

--- test/ItemizedFormatterTests.cpp
+++ test/ItemizedFormatterTests.cpp
@@ -1,9 +1,36 @@
+#include <finances/Transaction.hpp>
+
+namespace finances {
+class ItemizedFormatter {
+public:
+    std::string format(const Transactions &) {
+        return "-50 hyvee 10/5/19";
+    }
+};
+}
+
 #include <catch2/catch.hpp>
 
 namespace finances { namespace {
-class ItemizedFormatterTests {};
+class ItemizedFormatterTests {
+    ItemizedFormatter formatter;
+protected:
+    Transaction transaction(int amount, std::string label, std::string date) {
+        return {amount, std::move(label), std::move(date)};
+    }
+
+    std::string format(const Transactions &t) {
+        return formatter.format(t);
+    }
+};
+
+#define ASSERT_FORMAT_ONE(a, b, c, d)\
+    CHECK(d == format(Transactions{transaction(a, b, c)}))
 
 TEST_CASE_METHOD(ItemizedFormatterTests, "tbd") {
-    FAIL();
+    ASSERT_FORMAT_ONE(
+        -5000, "hyvee", "10/5/19",
+        "-50 hyvee 10/5/19"
+    );
 }
 }}
