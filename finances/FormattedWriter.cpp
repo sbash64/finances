521e21dfeb91cf16afdf9978aa64103cc33c8d6d
Seth Bashford <sbash64@gmail.com>
2019-10-10 09:24:15 -0500

    refactor

--- finances/FormattedWriter.cpp
+++ finances/FormattedWriter.cpp
@@ -5,11 +5,15 @@ FormattedWriter::FormattedWriter(Formatter &formatter, Writer &writer) :
     formatter{formatter},
     writer{writer} {}
 
+static void write(Writer &writer, const std::string &what) {
+    writer.write('\n' + what + "\n\n");
+}
+
 void FormattedWriter::printNetIncome(int x) {
-    writer.write('\n' + formatter.formatNetIncome(x) + "\n\n");
+    write(writer, formatter.formatNetIncome(x));
 }
 
 void FormattedWriter::printTransactions(const Transactions &t) {
-    writer.write('\n' + formatter.formatTransactions(t) + "\n\n");
+    write(writer, formatter.formatTransactions(t));
 }
 }
