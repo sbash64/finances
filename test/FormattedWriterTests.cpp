7ab398e18fa4c745ff312a9fbcbcc5c59afb8da3
Seth Bashford <sbash64@gmail.com>
2019-11-09 12:34:45 -0600

    refactor

--- test/FormattedWriterTests.cpp
+++ test/FormattedWriterTests.cpp
@@ -1,24 +1,21 @@
 #include "testing-utility.hpp"
-#include <finances/FormattedWriter.hpp>
 #include <catch2/catch.hpp>
+#include <finances/FormattedWriter.hpp>
 
-namespace finances { namespace {
+namespace finances {
+namespace {
 class FormatterStub : public Formatter {
-public:
-    auto transactionsToFormat() const {
-        return transactionsToFormat_;
-    }
+  public:
+    auto transactionsToFormat() const { return transactionsToFormat_; }
 
-    auto netIncomeToFormat() const {
-        return netIncomeToFormat_;
-    }
+    auto netIncomeToFormat() const { return netIncomeToFormat_; }
 
-    std::string formatNetIncome(int x) override {
+    auto formatNetIncome(int x) -> std::string override {
         netIncomeToFormat_ = x;
         return formattedNetIncome_;
     }
 
-    std::string formatTransactions(const Transactions &t) override {
+    auto formatTransactions(const Transactions &t) -> std::string override {
         transactionsToFormat_ = t;
         return formattedTransactions_;
     }
@@ -30,7 +27,8 @@ public:
     void setFormattedNetIncome(std::string s) {
         formattedNetIncome_ = std::move(s);
     }
-private:
+
+  private:
     Transactions transactionsToFormat_;
     std::string formattedTransactions_;
     std::string formattedNetIncome_;
@@ -38,38 +36,30 @@ private:
 };
 
 class WriterStub : public Writer {
-public:
-    auto written() const {
-        return written_;
-    }
+  public:
+    auto written() const { return written_; }
 
-    void write(const std::string &s) {
-        written_ = s;
-    }
-private:
+    void write(const std::string &s) override { written_ = s; }
+
+  private:
     std::string written_;
 };
 
 class FormattedWriterTests {
-protected:
-    Transactions transactionsToFormat() {
+  protected:
+    auto transactionsToFormat() -> Transactions {
         return formatter.transactionsToFormat();
     }
 
-    int netIncomeToFormat() {
-        return formatter.netIncomeToFormat();
-    }
+    auto netIncomeToFormat() -> int { return formatter.netIncomeToFormat(); }
 
     void showTransactions(const Transactions &t = {}) {
         printer.showTransactions(t);
     }
 
     void showOneTransaction(int amount, std::string label, std::string date) {
-        showTransactions(oneTransaction(
-            amount,
-            std::move(label),
-            std::move(date)
-        ));
+        showTransactions(
+            oneTransaction(amount, std::move(label), std::move(date)));
     }
 
     void setFormatted(std::string s) {
@@ -80,33 +70,27 @@ protected:
         formatter.setFormattedNetIncome(std::move(s));
     }
 
-    std::string written() {
-        return writer.written();
-    }
+    auto written() -> std::string { return writer.written(); }
 
-    void showNetIncome(int x = {}) {
-        printer.showNetIncome(x);
-    }
-private:
+    void showNetIncome(int x = {}) { printer.showNetIncome(x); }
+
+  private:
     FormatterStub formatter;
     WriterStub writer;
     FormattedWriter printer{formatter, writer};
 };
 
-#define ASSERT_ONE_TRANSACTION_TO_FORMAT(a, b, c)\
+#define ASSERT_ONE_TRANSACTION_TO_FORMAT(a, b, c)                              \
     ASSERT_EQUAL(oneTransaction(a, b, c), transactionsToFormat())
 
-#define ASSERT_WRITTEN(a)\
-    ASSERT_EQUAL(a, written())
+#define ASSERT_WRITTEN(a) ASSERT_EQUAL(a, written())
 
-#define ASSERT_WRITTEN_FOR_SHOWING(a)\
+#define ASSERT_WRITTEN_FOR_SHOWING(a)                                          \
     ASSERT_WRITTEN(std::string{"\n"} + a + "\n\n")
 
-#define ASSERT_NET_INCOME_TO_FORMAT(a)\
-    ASSERT_EQUAL(a, netIncomeToFormat())
+#define ASSERT_NET_INCOME_TO_FORMAT(a) ASSERT_EQUAL(a, netIncomeToFormat())
 
-#define FORMATTED_WRITER_TEST(a)\
-    TEST_CASE_METHOD(FormattedWriterTests, a)
+#define FORMATTED_WRITER_TEST(a) TEST_CASE_METHOD(FormattedWriterTests, a)
 
 FORMATTED_WRITER_TEST("showTransactionsFormatsOne") {
     showOneTransaction(-1000, "chipotle", "10/6/19");
@@ -129,4 +113,5 @@ FORMATTED_WRITER_TEST("showNetIncomeWritesNetIncome") {
     showNetIncome();
     ASSERT_WRITTEN_FOR_SHOWING("hello");
 }
-}}
+}
+}
