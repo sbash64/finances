ed5b94610715bd759b1c75d104c73e0ce6b1f25b
Seth Bashford <sbash64@gmail.com>
2020-03-29 09:11:54 -0500

    add history

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <finances/ItemizedFormatter.hpp>
 #include <finances/TransactionRecord.hpp>
 #include <readline/readline.h>
+#include <readline/history.h>
 #include <iostream>
 
 namespace finances {
@@ -12,14 +13,38 @@ class ConsoleWriter : public Writer {
     void write(const std::string &s) override { std::cout << s; }
 };
 
+class Readline {
+  public:
+    explicit Readline(const std::string &prompt)
+        : line_{readline(prompt.c_str())} {}
+
+    ~Readline() { free(line_); }
+
+    Readline(const Readline &) = delete;
+    auto operator=(const Readline &) -> Readline & = delete;
+    Readline(Readline &&) noexcept = delete;
+    auto operator=(Readline &&) noexcept -> Readline & = delete;
+
+    auto c_str() const -> const char * { return line_; }
+
+  private:
+    char *line_;
+};
+
+auto c_str(const Readline &line) -> const char * { return line.c_str(); }
+
 [[noreturn]] void main() {
     ConsoleWriter writer;
     ItemizedFormatter formatter;
     FormattedWriter formattedWriter{formatter, writer};
     TransactionRecord record;
     Presenter presenter{record, formattedWriter};
-    while (true)
-        presenter.execute(readline("finances$ "));
+    for (;;) {
+        Readline line("finances$ ");
+        presenter.execute(c_str(line));
+        if (c_str(line) != nullptr && *c_str(line) != 0)
+            add_history(c_str(line));
+    }
 }
 }
 }
