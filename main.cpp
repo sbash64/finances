cbca7bfc46d1ecb99bcf7562e20cd909f656eeff
Seth Bashford <sbash64@gmail.com>
2020-04-04 16:12:52 -0500

    refactor

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ class ConsoleWriter : public Writer {
 class Readline {
   public:
     explicit Readline(const std::string &prompt)
-        : line_{readline(prompt.c_str())} {}
+        : line{readline(prompt.c_str())} {}
 
-    ~Readline() { free(line_); }
+    ~Readline() { free(line); }
 
     Readline(const Readline &) = delete;
     auto operator=(const Readline &) -> Readline & = delete;
     Readline(Readline &&) noexcept = delete;
     auto operator=(Readline &&) noexcept -> Readline & = delete;
 
-    auto c_str() const -> const char * { return line_; }
+    auto c_str() const -> const char * { return line; }
 
   private:
-    char *line_;
+    char *line;
 };
 
 auto c_str(const Readline &line) -> const char * { return line.c_str(); }
