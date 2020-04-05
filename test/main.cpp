8ff9f23c9d47ea1e59984892f7c10b1c4e18236c
Seth Bashford <sbash64@gmail.com>
2020-04-05 18:48:48 -0500

    add tests to main

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,70 @@
-#define CATCH_CONFIG_MAIN
+#include "TransactionRecordTests.hpp"
+#include <testcpplite/testcpplite.hpp>
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+#include <iostream>
+
+namespace finances {
+int main() {
+    return testcpplite::test(
+        {{transactionRecordHasNoneOnConstruction,
+             "transactionRecordHasNoneOnConstruction"},
+            {transactionRecordNotifiesListenerOnAdd,
+                "transactionRecordNotifiesListenerOnAdd"},
+            {transactionRecordHasOneAdded, "transactionRecordHasOneAdded"},
+            {transactionRecordHasTwoAdded, "transactionRecordHasTwoAdded"},
+            {transactionRecordHasNoneAfterRemovingOne,
+                "transactionRecordHasNoneAfterRemovingOne"},
+            {transactionRecordHasThreeAdded, "transactionRecordHasThreeAdded"},
+            {transactionRecordHasNoneAfterRemovingNone,
+                "transactionRecordHasNoneAfterRemovingNone"},
+            {transactionRecordHasOneAfterRemovingOne,
+                "transactionRecordHasOneAfterRemovingOne"},
+            {transactionRecordHasTwoAfterRemovingOneNotFound,
+                "transactionRecordHasTwoAfterRemovingOneNotFound"},
+            {transactionRecordHasZeroNetIncomeOnConstruction,
+                "transactionRecordHasZeroNetIncomeOnConstruction"},
+            {transactionRecordHasOneContributingNetIncome,
+                "transactionRecordHasOneContributingNetIncome"},
+            {transactionRecordHasTwoContributingNetIncome,
+                "transactionRecordHasTwoContributingNetIncome"},
+            {transactionRecordHasNoneVerifiedOnConstruction,
+                "transactionRecordHasNoneVerifiedOnConstruction"},
+            {transactionRecordHasNoneVerifiedAfterAdd,
+                "transactionRecordHasNoneVerifiedAfterAdd"},
+            {transactionRecordHasOneVerifiedAfterAdd,
+                "transactionRecordHasOneVerifiedAfterAdd"},
+            {transactionRecordHasOneVerifiedAfterTwoAdded,
+                "transactionRecordHasOneVerifiedAfterTwoAdded"},
+            {transactionRecordHasTwoVerifiedAfterThreeAdded,
+                "transactionRecordHasTwoVerifiedAfterThreeAdded"},
+            {transactionRecordHasNoneVerifiedAfterVerify,
+                "transactionRecordHasNoneVerifiedAfterVerify"},
+            {transactionRecordDoesNotNotifyAfterOneAddAndVerify,
+                "transactionRecordDoesNotNotifyAfterOneAddAndVerify"},
+            {transactionRecordNotiesAfterOneAddAndVerify,
+                "transactionRecordNotiesAfterOneAddAndVerify"},
+            {transactionRecordHasNoneUnverifiedOnConstruction,
+                "transactionRecordHasNoneUnverifiedOnConstruction"},
+            {transactionRecordHasOneUnverifiedAfterAdd,
+                "transactionRecordHasOneUnverifiedAfterAdd"},
+            {transactionRecordHasOneUnverifiedAfterTwoAdded,
+                "transactionRecordHasOneUnverifiedAfterTwoAdded"},
+            {transactionRecordHasTwoVerifiedOfSameAmount,
+                "transactionRecordHasTwoVerifiedOfSameAmount"},
+            {transactionRecordHasOneVerifiedAfterAddingTwoOfSameAmount,
+                "transactionRecordHasOneVerifiedAfterAddingTwoOfSameAmount"},
+            {transactionRecordVerifiesMatchingAmountWhenRemovingFirst,
+                "transactionRecordVerifiesMatchingAmountWhenRemovingFirst"},
+            {transactionRecordVerifiesMatchingAmountWhenRemovingSecond,
+                "transactionRecordVerifiesMatchingAmountWhenRemovingSecond"},
+            {transactionRecordDoesNotVerifyMatchingAmountWhenRemovingOne,
+                "transactionRecordDoesNotVerifyMatchingAmountWhenRemovingOne"}},
+        std::cout);
+}
+}
+
+int main(int argc, char *argv[]) {
+    finances::main();
+    return Catch::Session().run(argc, argv);
+}
