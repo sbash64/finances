0094af59d3fc7a4ed24ebc6650bc640b7282d169
Seth Bashford <sbash64@gmail.com>
2020-05-25 13:26:42 -0500

    refactor

--- finances/include/finances/CommandResponder.hpp
+++ finances/include/finances/CommandResponder.hpp
@@ -1,29 +1,11 @@
-#ifndef FINANCES_INCLUDE_FINANCES_PRESENTER_HPP_
-#define FINANCES_INCLUDE_FINANCES_PRESENTER_HPP_
+#ifndef FINANCES_INCLUDE_FINANCES_COMMANDRESPONDER_HPP_
+#define FINANCES_INCLUDE_FINANCES_COMMANDRESPONDER_HPP_
 
 #include "Transaction.hpp"
+#include "Model.hpp"
 #include <string>
 
 namespace finances {
-class Model {
-  public:
-    class EventListener {
-      public:
-        virtual ~EventListener() = default;
-        virtual void verified(const Transaction &) = 0;
-        virtual void added(const Transaction &) = 0;
-    };
-    virtual ~Model() = default;
-    virtual void add(const Transaction &) = 0;
-    virtual void remove(const Transaction &) = 0;
-    virtual void verify(int amount) = 0;
-    virtual auto transactions() -> Transactions = 0;
-    virtual auto verifiedTransactions() -> Transactions = 0;
-    virtual auto unverifiedTransactions() -> Transactions = 0;
-    virtual auto netIncome() -> int = 0;
-    virtual void subscribe(EventListener *) = 0;
-};
-
 class View {
   public:
     virtual ~View() = default;
